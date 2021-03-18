7ccc43685057ebb73a5610e3274b1199828b51d5
Lucas Krul <lucas.krul@intergalaxy.io>
2021-03-18 16:41:03 -0300

    first commit

---
+++ App.js
@@ -0,0 +1,132 @@
+import * as React from 'react';
+import {
+  StatusBar,
+  FlatList,
+  Image,
+  Animated,
+  Text,
+  View,
+  Dimensions,
+  StyleSheet,
+  TouchableOpacity,
+  Easing,
+  SafeAreaViewBase,
+  SafeAreaView,
+} from 'react-native';
+const {width, height} = Dimensions.get('screen');
+import faker from 'faker';
+
+faker.seed(10);
+const DATA = [...Array(30).keys()].map((_, i) => {
+  return {
+    key: faker.random.uuid(),
+    image: `https://randomuser.me/api/portraits/${faker.helpers.randomize([
+      'women',
+      'men',
+    ])}/${faker.random.number(60)}.jpg`,
+    name: faker.name.findName(),
+    jobTitle: faker.name.jobTitle(),
+    email: faker.internet.email(),
+  };
+});
+
+const BG_IMG =
+  'https://images.pexels.com/photos/1231265/pexels-photo-1231265.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500';
+
+const SPACING = 20;
+const AVATAR_SIZE = 70;
+const ITEM_SIZE = AVATAR_SIZE + SPACING * 3;
+
+export default () => {
+  const scrollY = React.useRef(new Animated.Value(0)).current;
+
+  return (
+    <View style={{flex: 1, backgroundColor: '#fff'}}>
+      <StatusBar hidden />
+      <Image
+        source={{uri: BG_IMG}}
+        style={StyleSheet.absoluteFill}
+        blurRadius={80}
+      />
+      <Animated.FlatList
+        data={DATA}
+        showsVerticalScrollIndicator={false}
+        onScroll={Animated.event(
+          [{nativeEvent: {contentOffset: {y: scrollY}}}],
+          {useNativeDriver: true},
+        )}
+        keyExtractor={item => item.key}
+        contentContainerStyle={{
+          padding: SPACING,
+          paddingTop: StatusBar.currentHeight || 42,
+        }}
+        renderItem={({item, index}) => {
+          const inputRange = [
+            -1,
+            0,
+            ITEM_SIZE * index,
+            ITEM_SIZE * (index + 2),
+          ];
+
+          const opacityInputRange = [
+            -1,
+            0,
+            ITEM_SIZE * index,
+            ITEM_SIZE * (index + 0.5),
+          ];
+
+          const scale = scrollY.interpolate({
+            inputRange,
+            outputRange: [1, 1, 1, 0],
+          });
+
+          const opacity = scrollY.interpolate({
+            inputRange: opacityInputRange,
+            outputRange: [1, 1, 1, 0],
+          });
+
+          return (
+            <Animated.View
+              style={{
+                flexDirection: 'row',
+                padding: SPACING,
+                marginBottom: SPACING,
+                borderRadius: 12,
+                backgroundColor: 'rgba(255, 255, 255, 0.8)',
+                shadowColor: '#000',
+                shadowOffset: {
+                  width: 0,
+                  height: 10,
+                },
+                shadowOpacity: 0.3,
+                shadowRadius: 20,
+                opacity,
+                transform: [{scale}],
+              }}>
+              <Image
+                source={{uri: item.image}}
+                style={{
+                  width: AVATAR_SIZE,
+                  height: AVATAR_SIZE,
+                  borderRadius: AVATAR_SIZE,
+                  marginRight: SPACING / 2,
+                }}
+              />
+              <View>
+                <Text style={{fontSize: 18, fontWeight: '700'}}>
+                  {item.name}
+                </Text>
+                <Text style={{fontSize: 14, opacity: 0.7}}>
+                  {item.jobTitle}
+                </Text>
+                <Text style={{fontSize: 12, opacity: 0.8, color: '#0099cc'}}>
+                  {item.email}
+                </Text>
+              </View>
+            </Animated.View>
+          );
+        }}
+      />
+    </View>
+  );
+};
---
+++ README.md
@@ -0,0 +1,25 @@
+<p align="center">
+  <a href="#rocket-tecnologias">Tecnologias</a>&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;
+  <a href="#-como-excutar">Como excutar</a>
+</p>
+
+<br>
+
+<p align="center">
+  <img alt="Scroll" src=".github/scroll.gif" width="20%">
+</p>
+
+## :rocket: Tecnologias
+
+Esse projeto foi desenvolvido com a finalidade apenas para estudos de animações feitas com a tecnologia:
+
+- [React Native](https://facebook.github.io/react-native/)
+
+## 🤔 Como excutar
+
+- Faça o clone ou download do projeto;
+- Acesse as pastas separadamente e execute: `npm install`;
+- Em cada pasta sera necessário executar: `npm start` para rodar o projeto;
+
+
+Feito com ♥ e ☕ by Lucas Krul 
